963497dcff1dc5d2c9a63a7a3057f8d1a6ce9ded
jim <16655467002@163.com>
2022-06-06 13:45:21 +0800

    定义视图函数

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+'''
+所谓的视图，就是由python函数编写的
+视图函数有两个要求：
+   1.接收请求
+   2.返回一个响应
+'''
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+# 期望用户输入 http://127.0.0.1:8000/index/
+# 来访问视图函数
+
+
+def index(request):
+    return HttpResponse('ok')
